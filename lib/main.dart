05411c6cd61e863c375f5b749c0f4c9275f5f748
Admin <admin@gmail.com>
2024-06-12 18:24:31 +0500

    second commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'dart:ui' show PointerDeviceKind;
+import 'package:flutter/material.dart';
+import 'package:e_commerce_flutter/core/app_theme.dart';
+import 'package:e_commerce_flutter/src/view/screen/home_screen.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+    
+      scrollBehavior: const MaterialScrollBehavior().copyWith(
+        dragDevices: {
+          PointerDeviceKind.mouse,
+          PointerDeviceKind.touch,
+        },
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const HomeScreen(),
+      theme: AppTheme.lightAppTheme,
+    );
+  }
+}
---
+++ lib/src/view/screen/profile_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [ 
+          Expanded(child: Image.asset('assets/images/profile_pic.png')),
+          const Text( 
+            "Hello!",
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('assets/images/github.png', width: 60),
+              const SizedBox(width: 10),
+              const Text(
+                "https://",
+                style: TextStyle(fontSize: 20),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
